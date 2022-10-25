bc0245069fb42fb1077de1ca068701cf8dda9764
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-25 22:51:36 +0600

    firebase config added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG1kirkPLAO_RtD-Q1PdwA9vzHGZozTvk",
+  authDomain: "e-learning-website-client.firebaseapp.com",
+  projectId: "e-learning-website-client",
+  storageBucket: "e-learning-website-client.appspot.com",
+  messagingSenderId: "37269428939",
+  appId: "1:37269428939:web:514fc5234169e33875594e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
